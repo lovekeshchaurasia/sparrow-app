2e84632793f2933d06f7e75887e8e5a745f96798
lovekesh <officiallovekesh094@gmail.com>
2025-03-11 13:59:29 +0530

    member-workspace

--- packages/@sparrow-teams/src/features/team-explorer/layout/TeamExplorer.svelte
+++ packages/@sparrow-teams/src/features/team-explorer/layout/TeamExplorer.svelte
@@ -11,7 +11,7 @@
   import { Button } from "@sparrow/library/ui";
   import { Navigator } from "@sparrow/library/ui";
   import { Avatar } from "@sparrow/library/ui";
-  import { ListRegular } from "@sparrow/library/icons";
+
   import {
     TeamTabsEnum,
     TeamViewEnum,
@@ -318,7 +318,7 @@
             style="padding-bottom: 4px;"
           >
             <Navigator
-              tabs={teamTabs}
+              tabs={teamTabs.filter((tab) => tab.visible !== false)}
               currentTabId={"Workspaces"}
               onTabClick={onUpdateActiveTab}
               {activeTeamTab}
@@ -336,17 +336,15 @@
                   alt=""
                 />
               </span>
-
-              <span
-                style="cursor:pointer;display:flex;justify-content:center;align-items:center"
-                on:click={() => {
-                  workspaceView.set(TeamViewEnum.LIST);
-                }}
-                class:view-active={selectedView === TeamViewEnum.LIST}
-                src={hamburger}
-                alt=""
-              >
-                <ListRegular size={"16px"} />
+              <span style="cursor:pointer;">
+                <img
+                  on:click={() => {
+                    workspaceView.set(TeamViewEnum.LIST);
+                  }}
+                  class:view-active={selectedView === TeamViewEnum.LIST}
+                  src={hamburger}
+                  alt=""
+                />
               </span>
             {/if}
           </div>
@@ -529,8 +527,5 @@
   }
   .moreOption-icon:hover {
     background-color: var(--bg-tertiary-190);
-  }
-  .teams-menu__right {
-    display: flex;
   }
 </style>
